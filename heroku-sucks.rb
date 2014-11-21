cb2312c37f31f1a8725e3a22e02a7079e95c149a
Ryan Northrup <rnorthrup@newleaders.com>
2014-11-20 17:04:13 -0800

    Because apparently implementing proper user switching is too hard for Heroku.

--- heroku-sucks.rb
+++ heroku-sucks.rb
@@ -2,12 +2,20 @@ require 'rubygems'
 require 'sinatra/base'
 
 class HerokuSucksApp < Sinatra::Application
+  set :haml, :format => :html5
+  
   get '/realzies' do
     send_file 'realzies.png'
   end
   
   get '/:name' do
-    "#{params['name']} sucks."
+    haml <<END
+%html
+  %head
+    %title #{params[:name]} sucks.
+  %body
+    %h1 #{params[:name]} sucks.
+END
   end
   
   get '/' do
